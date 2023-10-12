c0c5a4d0d67c7b7d3875ded0dc2de36c30155c31
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-12 11:57:34 -0300

    changes styles

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -15,14 +15,14 @@ const Data = () => {
   }, 0)
 
   return (
-    <div className="flex flex-row justify-between mx-auto w-full max-w-3xl mb-4">
-      <p className="font-bold text-[#DEC74E] text-sm">
+    <div className="flex flex-col sm:flex-row justify-between mx-auto w-full max-w-3xl mb-4">
+      <p className="font-bold text-[#DEC74E] text-sm mt-4">
         Tasks created{' '}
         <span className="bg-[#262626] py-1 px-2 rounded-l-full rounded-r-full text-white ml-2">
           {totalTasks}
         </span>
       </p>
-      <p className="font-bold text-[#696BFE] text-sm">
+      <p className="font-bold text-[#696BFE] text-sm mt-4">
         Completed{' '}
         <span className="bg-[#262626] py-1 px-2 rounded-l-full rounded-r-full text-white ml-2">
           {countCompletedTasks} de {totalTasks}
