bbe4540a973fb8912d0dbd996f4d3e8b09a21885
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-12 10:53:22 -0300

    add TasksActions types

--- src/types/tasksTypes.ts
+++ src/types/tasksTypes.ts
@@ -5,3 +5,8 @@ export interface Task {
 }
 
 export type TasksState = Task[]
+
+export type TasksAction =
+  | { type: 'ADD_TODO'; id: string; text: string }
+  | { type: 'CHANGE_TODO'; task: Task }
+  | { type: 'DELETE_TODO'; id: string }
