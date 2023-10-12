5c108169b25d9aa3e3023d5dbd3622d1d6b6a5af
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-12 11:25:30 -0300

    add custom hooks to use contexts

--- src/context/TasksContext.ts
+++ src/context/TasksContext.ts
@@ -1,7 +1,23 @@
-import { createContext } from 'react'
+import { createContext, useContext } from 'react'
 import { TasksAction, TasksState } from '../types/tasksTypes'
 
 export const TasksContext = createContext<TasksState | null>(null)
 export const TasksDispatchContext = createContext<React.Dispatch<TasksAction> | null>(
   null
 )
+
+export function useTasks () {
+  const tasks = useContext(TasksContext)
+  if (tasks === null) {
+    throw new Error('useTasks must be used within a TasksProvider')
+  }
+  return tasks
+}
+
+export function useTasksDispatch () {
+  const dispatch = useContext(TasksDispatchContext)
+  if (dispatch === null) {
+    throw new Error('useTasksDispatch must be used within a TasksProvider')
+  }
+  return dispatch
+}
